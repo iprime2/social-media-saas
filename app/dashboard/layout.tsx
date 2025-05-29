7dc60630f9bed45ce7bf5ab78a4e8ff9c00590ed
Sushil Gupta <kalwarsushil2@gmail.com>
2025-05-29 14:32:55 +0530

    Fix theme hydration mismatch

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { Navbar } from "@/components/navbar";
-import { ThemeProvider } from "@/components/theme-provider";
 
 export default function DashboardLayout({
   children,
@@ -11,10 +10,7 @@ export default function DashboardLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <main className="flex-1 p-6">{children}</main>
-        </ThemeProvider>
+      <main className="flex-1 p-6">{children}</main>
     </div>
   );
 }
